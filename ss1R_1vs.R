52c23707c07dbff23049dff361c486c6e31ecbb0
Adriana Saenz <adsaenzos@gmail.com>
2025-08-27 14:08:04 -0500

    Create ss1R_1vs.R

---
+++ ss1R_1vs.R
@@ -0,0 +1,12 @@
+#First session - R studio
+
+library(dplyr)
+library(rvest)
+library(polite)
+library(stringr)
+library(tidyr)
+library(readxl)
+library(janitor)
+
+#carga de datos
+data <- read.csv(".csv")
